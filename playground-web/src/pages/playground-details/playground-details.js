2fc379a258ddb7c98421cf3ca9e222e986b0d6e8
A.Dugelet <du.aubin@hotmail.fr>
2018-12-21 10:03:43 +0100

    web : adding app routing

---
+++ playground-web/src/pages/playground-details/playground-details.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class PlaygroundDetails extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Playground details</h1>
+            </div>
+        )
+    }
+}
