ad3878033ee219b762fcb59d404e76479b5e7ae1
A.Dugelet <du.aubin@hotmail.fr>
2018-12-21 10:03:43 +0100

    web : adding app routing

---
+++ playground-web/src/pages/sign-in/sign-in.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export default class SignIn extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>SignIn</h1>
+            </div>
+        )
+    }
+}
