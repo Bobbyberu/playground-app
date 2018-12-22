d5126305e91da00b53904a8a7a86af4e0b68a5c3
A.Dugelet <du.aubin@hotmail.fr>
2018-12-22 09:35:39 +0100

    fullscreen map and current localisation

--- playground-web/src/App.js
+++ playground-web/src/App.js
@@ -7,6 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <AppRouter />
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css"
+          integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
+          crossOrigin="" />
       </div>
     );
   }
--- playground-web/src/pages/home/home.js
+++ playground-web/src/pages/home/home.js
@@ -1,11 +1,38 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import './home.css';
+var Tools = require('../../services/tools');
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            location: undefined,
+            zoom: 15
+        }
+    }
+
+    componentDidMount() {
+        let currentLocation;        
+        Tools.getLocation(function(position) {
+            currentLocation = [position.coords.latitude, position.coords.longitude];
+            this.setState({
+                location: currentLocation
+            });
+        }.bind(this));
+    }
+
     render() {
         return (
-            <div>
-                <h1>Home</h1>
-            </div>
-        )
+            <Map center={this.state.location} zoom={this.state.zoom} className="fullscreen">
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                />
+                <Marker position={this.state.location}>
+                    <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+                </Marker>
+            </Map>
+        );
     }
 }
