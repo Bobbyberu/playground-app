38faae6f1ba0ee6ecc4df447c1c866911aab85a8
Zeruome <zeruome@gmail.com>
2019-01-01 15:01:59 +0100

    working progress

--- playground-web/src/pages/home/home.js
+++ playground-web/src/pages/home/home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import PlaygroundDetails from '../playground-details/playground-details';
 import './home.css';
 var Tools = require('../../services/tools');
 
@@ -8,31 +9,54 @@ export default class Home extends Component {
         super(props)
         this.state = {
             location: undefined,
-            zoom: 15
+            zoom: 15,
+            name: '',
+            adress: '',
         }
     }
 
     componentDidMount() {
-        let currentLocation;        
+        let currentLocation, currentName, currentAdress;
         Tools.getLocation(function(position) {
             currentLocation = [position.coords.latitude, position.coords.longitude];
             this.setState({
                 location: currentLocation
             });
         }.bind(this));
+
+        // Récupérer les propriétés lieés au terrain du marqueur, pour l'instant de la position courante
+        this.setState({
+            name: 'Abdel la cisah akay limpulsif',
+            adress: '53 rue des pruniers 69007 Lyon',
+        })
+    }
+
+    handleClick(){
+
     }
 
     render() {
         return (
-            <Map center={this.state.location} zoom={this.state.zoom} className="fullscreen">
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                />
-                <Marker position={this.state.location}>
-                    <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
-                </Marker>
-            </Map>
+            <div>
+                <Map center={this.state.location} zoom={this.state.zoom} className="fullscreen">
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                    />
+                    <Marker position={this.state.location}>
+                        <Popup>
+                            {/* Afficher le composant détails du playground avec les props associées au marqueur*/}
+                            <PlaygroundDetails
+                                name={this.state.name}
+                                adress={this.state.adress}
+                            />
+                        </Popup>
+                    </Marker>
+                </Map>
+                <button onClick={this.handleClick}>
+                    Add playground
+                </button>
+            </div>
         );
     }
 }
