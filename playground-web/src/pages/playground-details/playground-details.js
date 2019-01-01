6847aaa5a3af42db1ceea5f9bef6488b9db713fb
Zeruome <zeruome@gmail.com>
2019-01-01 15:01:59 +0100

    working progress

--- playground-web/src/pages/playground-details/playground-details.js
+++ playground-web/src/pages/playground-details/playground-details.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
+import img from '../../pictures/playground.jpg';
+import './playground-details.css';
 
-export default class PlaygroundDetails extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Playground details</h1>
-            </div>
-        )
-    }
-}
+const PlaygroundDetails = ({ name, adress }) => (
+    <div className="global">
+        <div className="header">
+            <h1>{name}</h1>
+            <img src={img}/>
+        </div>
+        <div className="leftSide">
+            <span className="label location">Location : {adress}</span>
+        </div>
+        <div className="rightSide">
+
+        </div>
+    </div>
+)
+
+export default PlaygroundDetails
