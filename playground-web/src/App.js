7c7755254f15ba8ed3af1934572c7a869d088e79
Zeruome <zeruome@gmail.com>
2019-01-01 23:44:42 +0100

    ajout d'une barre de menu

--- playground-web/src/App.js
+++ playground-web/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import AppRouter from './AppRouter';
 import './App.css';
+import NavBar from "./common-components/nav-bar/nav-bar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <AppRouter />
+        <NavBar/>
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css"
           integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
           crossOrigin="" />
