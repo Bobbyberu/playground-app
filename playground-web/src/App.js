cf1f79f7eead84da13a18470fff01e425fb9696b
Zeruome <zeruome@gmail.com>
2019-01-11 10:54:09 +0100

    Mise en place de l'architecture redux et premier reducer (favoris)
    * Mise en place de l'architecture redux et premier reducer (favoris)
    
    * refactor folder's name

--- playground-web/src/App.js
+++ playground-web/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import AppRouter from './AppRouter';
 import './App.css';
+import { Provider } from 'react-redux'
+import Store from './store/configureStore'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <AppRouter />
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css"
-          integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
-          crossOrigin="" />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Provider store={Store}>
+				<div className="App">
+					<AppRouter />
+					<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css"
+						integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
+						crossOrigin="" />
+				</div>
+			</Provider>
+		);
+	}
 }
 
 export default App;
