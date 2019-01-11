dd16728377402dde77e6c87b9ba185cb56c9c5fb
Dylan Martins <dydy.martins@free.fr>
2019-01-11 15:59:31 +0100

    Update Hours in Schedule entity

--- spring/src/main/java/com/playground/repository/ScheduleRepository.java
+++ spring/src/main/java/com/playground/repository/ScheduleRepository.java
@@ -1,10 +1,10 @@
 package com.playground.repository;
 
-import com.playground.model.Hours;
+import com.playground.model.Schedule;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HoursRepository extends CrudRepository<Hours, Integer> {
+public interface ScheduleRepository extends CrudRepository<Schedule, Integer> {
 
 }
