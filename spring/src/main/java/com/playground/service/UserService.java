092c471e9a00d9bc1de8150eb0bc710993a17df3
Dylan MARTINS <dylan.martins@fiducial.net>
2019-01-15 13:19:21 +0100

    Create user service

---
+++ spring/src/main/java/com/playground/service/UserService.java
@@ -0,0 +1,64 @@
+package com.playground.service;
+
+import com.playground.model.User;
+import com.playground.repository.UserRepository;
+import com.playground.service.interfaces.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class UserService
+ */
+@Service
+public class UserService implements IUserService {
+
+    /** UserRepository userRepository */
+    private final UserRepository userRepository;
+
+    /**
+     * UserService Constructor
+     *
+     * @param userRepository UserRepository
+     */
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+
+        return users;
+    }
+
+    @Override
+    public User getUser(int id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username).orElse(null);
+    }
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(int id, User user) {
+        user.setId(id);
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(User user) {
+        userRepository.delete(user);
+    }
+}
