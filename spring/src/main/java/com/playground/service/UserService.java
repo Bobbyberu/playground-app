0610e6a2836d425a183e4f444e9cbc4e70e4839d
Julien DUCROT <julien.ducrot@mooreacreations.com>
2019-01-21 22:55:16 +0100

    Add profile modification page + comment refacto

--- spring/src/main/java/com/playground/service/UserService.java
+++ spring/src/main/java/com/playground/service/UserService.java
@@ -117,6 +117,10 @@ public class UserService implements IUserService {
         return userRepository.findByUsername(username).orElse(null);
     }
 
+    public User getUserByMail(String mail) {
+        return userRepository.findByMail(mail).orElse(null);
+    }
+
     @Override
     public User getUserByMail(String mail) {
         return userRepository.findByMail(mail).orElse(null);
