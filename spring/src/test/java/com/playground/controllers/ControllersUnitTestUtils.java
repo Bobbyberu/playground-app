41029a65c2c9d2040d4d3fa75bfe16ef373ea268
A.Dugelet <aubin.dugelet@gmail.com>
2019-03-13 20:47:56 +0100

    refacto reportcomment unit tests :white_check_mark:

--- spring/src/test/java/com/playground/controllers/ControllersUnitTestUtils.java
+++ spring/src/test/java/com/playground/controllers/ControllersUnitTestUtils.java
@@ -2,6 +2,7 @@ package com.playground.controllers;
 
 import com.playground.model.entity.Comment;
 import com.playground.model.entity.Playground;
+import com.playground.model.entity.ReportComment;
 import com.playground.model.entity.Schedule;
 import com.playground.model.entity.Sport;
 import com.playground.model.entity.User;
@@ -54,6 +55,16 @@ public class ControllersUnitTestUtils {
         when(comment.getAuthor()).thenReturn(user);
 
         return comment;
+    }
+
+    public static ReportComment buildReportComment(Comment comment, User author) {
+        ReportComment reportComment = mock(ReportComment.class);
+
+        when(reportComment.getComment()).thenReturn(comment);
+        when(reportComment.getAuthor()).thenReturn(author);
+        when(reportComment.getDescription()).thenReturn("description");
+
+        return reportComment;
     }
 
     public static Set<Schedule> buildSchedules() {
